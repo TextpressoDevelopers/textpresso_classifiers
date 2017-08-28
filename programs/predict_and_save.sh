3115c1bd174a6ac40b23cc570a5c4b81c98d9706
Valerio Arnaboldi <valearna@caltech.edu>
2017-08-28 11:08:21 -0700

    new directory structure

--- programs/predict_and_save.sh
+++ programs/predict_and_save.sh
@@ -1,16 +1,59 @@
 #!/usr/bin/env bash
 
-model_dir=$1
-prediction_dir=$2
+function usage {
+    echo "usage: $(basename $0) <model_dir> <data_dir>"
+    echo "  -h --help                display help"
+    exit 1
+}
+
+if [[ "${#}" < 2 ]]
+then
+    usage
+fi
+
+model_dir=""
+data_dir=""
+
+while [[ $# -gt 1 ]]
+do
+key=$1
+
+case $key in
+    -h|--help)
+    usage
+    ;;
+    *)
+    if [[ -d $key ]]
+    then
+        model_dir="$key"
+    else
+        usage
+    fi
+    shift
+    if [ -d $1 ]
+    then
+        data_dir=$1
+    else
+        usage
+    fi
+    shift
+    ;;
+esac
+done
+
+if [[ ${model_dir} == "" || ${data_dir} == "" ]]
+then
+    usage
+fi
 
 for datatype in $(ls ${model_dir})
 do
     if [ -d ${model_dir}/${datatype} ]
     then
-        ./boost_classifier.py -p ${prediction_dir}/${datatype}/vpos_tp -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${prediction_dir}/${datatype}/prediction_vpos_tp.csv &
-        ./boost_classifier.py -p ${prediction_dir}/${datatype}/vpos_fp -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${prediction_dir}/${datatype}/prediction_vpos_fp.csv &
-        ./boost_classifier.py -p ${prediction_dir}/${datatype}/vneg_tn -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${prediction_dir}/${datatype}/prediction_vneg_tn.csv &
-        ./boost_classifier.py -p ${prediction_dir}/${datatype}/vneg_fn -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${prediction_dir}/${datatype}/prediction_vneg_fn.csv &
+        ./boost_classifier.py -p ${data_dir}/${datatype}/vpos_tp -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${data_dir}/${datatype}/prediction_vpos_tp.csv &
+        ./boost_classifier.py -p ${data_dir}/${datatype}/vpos_fp -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${data_dir}/${datatype}/prediction_vpos_fp.csv &
+        ./boost_classifier.py -p ${data_dir}/${datatype}/vneg_tn -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${data_dir}/${datatype}/prediction_vneg_tn.csv &
+        ./boost_classifier.py -p ${data_dir}/${datatype}/vneg_fn -c ${model_dir}/${datatype}/model.pkl -f "cas_pdf" > ${data_dir}/${datatype}/prediction_vneg_fn.csv &
     fi
 done
 wait
---
+++ tools/calculate_precision_recall_stored_model_on_curationstats.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+### calculate estimated precision and recall values on data obtained from previously calculated predictions by balancing
+### the validation and assuming that the curator should have evaluated an equal number of positive and negative results
+### according to the numbers obtained from the curation status page
+
+stored_predictions_root_dir=$1
+datatypes=("antibody" "catalyticact" "expression_cluster" "geneint" "geneprod" "genereg" "newmutant" "otherexpr" \
+"overexpr" "rnai" "seqchange" "structcorr")
+dtindices=(1 3 5 7 8 9 14 16 17 19 20 22)
+
+tmpfile=$(mktemp)
+wget -o /dev/null --post-data="select_curator=two736&action=Curation+Statistics+Page&checkbox_all_datatypes=all&checkbox_all_flagging_methods=all" "http://tazendra.caltech.edu/~postgres/cgi-bin/curation_status.cgi" -O ${tmpfile}
+
+echo -e "datatype\tprecision\trecall\tpn_rate"
+
+for ((i=0; i<$((${#datatypes[@]})); i++))
+do
+    idx=$((${dtindices[$i]} + 1))
+    tot_p=0
+    tmp=$(grep -o -P "SVM positive any</a>.*?</tr>" ${tmpfile} | awk -v col=${idx} 'BEGIN{FS="<td colspan=\"1\">"} {print $col}' | grep -oP ">\K[0-9]*")
+    if [[ ${tmp} != "" ]]
+    then
+        tot_p=${tmp}
+    fi
+    tot_vp=0
+    tmp=$(grep -o -P "SVM positive any validated</a>.*?</tr>" ${tmpfile} | awk -v col=${idx} 'BEGIN{FS="<td colspan=\"1\">"} {print $col}' | grep -oP ">\K[0-9]*")
+    if [[ ${tmp} != "" ]]
+    then
+        tot_vp=${tmp}
+    fi
+    tot_n=0
+    tmp=$(grep -o -P "SVM negative</a>.*?</tr>" ${tmpfile} | awk -v col=${idx} 'BEGIN{FS="<td colspan=\"1\">"} {print $col}' | grep -oP ">\K[0-9]*")
+    if [[ ${tmp} != "" ]]
+    then
+        tot_n=${tmp}
+    fi
+    tot_vn=0
+    tmp=$(grep -o -P "SVM negative validated</a>.*?</tr>" ${tmpfile} | awk -v col=${idx} 'BEGIN{FS="<td colspan=\"1\">"} {print $col}' | grep -oP ">\K[0-9]*")
+    if [[ ${tmp} != "" ]]
+    then
+        tot_vn=${tmp}
+    fi
+    tp=$(awk '{if ($2 == 1)}' ${stored_predictions_root_dir}/${datatypes[$i]}/prediction_vpos_tp.csv | wc -l | awk '{print $1}')
+    fp=$(awk '{if ($2 == 1)}' ${stored_predictions_root_dir}/${datatypes[$i]}/prediction_vpos_fp/.csv | wc -l | awk '{print $1}')
+    tn=$(awk '{if ($2 == 1)}' ${stored_predictions_root_dir}/${datatypes[$i]}/predicstion_vneg_tn.csv | wc -l | awk '{print $1}')
+    fn=$(awk '{if ($2 == 1)}' ${stored_predictions_root_dir}/${datatypes[$i]}/prediction_vneg_fn.csv | wc -l | awk '{print $1}')
+
+    if [[ ${tot_vn} != "0" ]]
+    then
+        pn_rate=$(echo "("${tot_n}"*"${tot_vp}")/("${tot_vn}"*"${tot_p}")" | bc -l)
+        tn=$(echo ${tn}"*"${pn_rate} | bc -l)
+        fn=$(echo ${fn}"*"${pn_rate} | bc -l)
+
+        precision=0
+        if [[ $(echo ${tp}"+"${fp}">0" | bc -l) != "0" ]]; then precision=$(echo ${tp}"/("${tp}"+"${fp}")" | bc -l); fi
+        recall=0
+        if [[ $(echo ${tp}"+"${fn}">0" | bc -l) != "0" ]]; then recall=$(echo ${tp}"/("${tp}"+"${fn}")" | bc -l); fi
+
+        echo -e ${datatypes[$i]}"\t"${precision}"\t"${recall}"\t"${pn_rate}
+    else
+        echo -e ${datatypes[$i]}"\tNA\tNA\tNA"
+    fi
+done
+
+rm ${tmpfile}
+
