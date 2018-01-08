a18013a969ba09ebfd996c732b9f62dbc5c75f42
Valerio Arnaboldi <valearna@caltech.edu>
2018-01-08 15:34:59 -0800

    added tokenization scheme argument to training scripts

--- tools/train_only_all_datatypes.sh
+++ tools/train_only_all_datatypes.sh
@@ -5,6 +5,7 @@ function usage {
     echo "  -t --type                set the file type. Accepted values are pdf or cas"
     echo "  -n --ngram-size          set the n-gram size"
     echo "  -m --max-features        set the maximum number of best features to be kept for feature selection"
+    echo "  -z --tokenization-scheme type of tokenization to apply to extract the feature set. Accepted values are TFIDF or BOW (Bag of Words)"
     echo "  -h --help                display help"
     exit 1
 }
@@ -13,6 +14,7 @@ INPUT_DIR=""
 FILE_TYPE="pdf"
 NGRAM_SIZE="2"
 MAX_FEATURES="20000"
+TOKENIZATION="TFIDF"
 
 models=("KNN" "SVM_LINEAR" "SVM_NONLINEAR" "TREE" "RF" "MLP" "NAIVEB" "GAUSS" "LDA" "XGBOOST")
 
@@ -48,6 +50,11 @@ case $key in
     MAX_FEATURES="$1"
     shift
     ;;
+    -z|--tokenization-scheme)
+    shift
+    TOKENIZATION="$1"
+    shift
+    ;;
     -h|--help)
     usage
     ;;
@@ -74,7 +81,7 @@ do
     then
         for model in ${models[@]}
         do
-            tp_doc_classifier.py -t ${INPUT_DIR}/${datatype} -c ${INPUT_DIR}/${datatype}/${model}.pkl -f ${FILE_TYPE} -m ${model} -n ${NGRAM_SIZE} -b ${MAX_FEATURES} -z TFIDF &
+            tp_doc_classifier.py -t ${INPUT_DIR}/${datatype} -c ${INPUT_DIR}/${datatype}/${model}.pkl -f ${FILE_TYPE} -m ${model} -n ${NGRAM_SIZE} -b ${MAX_FEATURES} -z ${TOKENIZATION} &
         done
     fi
     wait
