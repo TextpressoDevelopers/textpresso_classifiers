e29bd72b1ba9cca499315e924464e54c39b57e9c
Valerio Arnaboldi <valearna@caltech.edu>
2018-01-23 16:55:50 -0800

    added classification pipeline script
    - to be tested

--- wormbase_tools/tp_classification_pipeline.sh
+++ wormbase_tools/tp_classification_pipeline.sh
@@ -5,17 +5,18 @@ function usage {
     echo "<datatype>.pkl (lowercase). The data dir must contain two sub-folders: 'PMCOA' and 'C. elegans', "
     echo "each of which must contain sub-forlders with paper names, with cas files in them."
     echo ""
-    echo "usage: $(basename $0)      <models_dir> <data_dir>"
+    echo "usage: $(basename $0)      <models_dir> <data_dir> <output_dir>"
     echo "  -h --help                display help"
     exit 1
 }
 
 MODELS_DIR=""
 DATA_DIR=""
-FILE_TYPE=""
+OUTPUT_DIR=""
 
 datatypes=("antibody" "catalyticact" "expression_cluster" "geneint" "geneprod" "genereg" "newmutant" "otherexpr" \
 "overexpr" "rnai" "seqchange" "structcorr")
+filetypes=("C. elegans" "PMCOA")
 
 while [[ $# -gt 0 ]]
 do
@@ -40,6 +41,13 @@ case $key in
         usage
     fi
     shift
+    if [[ -d $key ]]
+    then
+        OUTPUT_DIR="$key"
+    else
+        usage
+    fi
+    shift
     ;;
 esac
 done
@@ -49,9 +57,45 @@ then
     usage
 fi
 
-#
+mkdir -p "${OUTPUT_DIR}/C. elegans/tmp"
+touch "${OUTPUT_DIR}/C. elegans/already_classified.csv"
+mkdir -p "${OUTPUT_DIR}/PMCOA/tmp"
+touch "${OUTPUT_DIR}/PMCOA/already_classified.csv"
 
-for datatype in $datatypes[@]
+for filetype in $filetypes[@]
 do
-    tp_doc_classifier.py -c ${MODELS_DIR}/${datatype}.pkl -p /
+    if [[ ${filetype} == "C. elegans" ]]
+    then
+        cas_type="cas_pdf"
+    else
+        cas_type="cas_xml"
+    fi
+    for datatype in $datatypes[@]
+    do
+        diff <(ls "${DATA_DIR}/${filetype}") <(cat "${OUTPUT_DIR}/${filetype}/already_classified.csv") | grep "^< " | sed 's/< //g' > "${OUTPUT_DIR}/${filetype}/tobeclassified.csv"
+        num_papers=0
+        cat "${OUTPUT_DIR}/${filetype}/tobeclassified.csv" | while read line
+        do
+            find "${DATA_DIR}/${filetype}/${line}/" -name "*.tpcas.gz" | xargs -I {} cp {} "${OUTPUT_DIR}/${filetype}/tmp/${line}.tpcas.gz"
+            if [[ ${filetype} == "C. elegans" ]]
+            then
+                ls "${DATA_DIR}/C. elegans Supplementals/${line}"* | while read sup
+                do
+                    find "$sup" -name "*.tpcas.gz" | xargs -I {} cp {} "${OUTPUT_DIR}/${filetype}/tmp/${sup}.tpcas.gz"
+                done
+            fi
+            find "${OUTPUT_DIR}/${filetype}/tmp/" -name "${line}*.tpcas.gz" | xargs -I {} sh -c 'convert_to_txt.py -f ${cas_type} "{}" > "${OUTPUT_DIR}/${filetype}/tmp/$(echo {} | sed 's/.tpcas.gz/.txt/g')"'
+            find "${OUTPUT_DIR}/${filetype}/tmp/" -name "${line}*.pdf" | xargs rm
+            cat "${OUTPUT_DIR}/${filetype}/tmp/${line}"* > "${OUTPUT_DIR}/${filetype}/tmp/${line}.concat.txt"
+            find "${OUTPUT_DIR}/${filetype}/tmp/" -name "${line}*.txt" | grep -v ".concat.txt" | xargs rm
+            if [[ $(echo "${num_papers}%1000" | bc) == "0" ]]
+            echo ${line} >> "${OUTPUT_DIR}/${filetype}/already_classified.csv"
+            then
+                tp_doc_classifier.py -c ${MODELS_DIR}/${datatype}.pkl -p "${OUTPUT_DIR}/${filetype}/tmp" -f ${cas_type} >> "${OUTPUT_DIR}/${filetype}/$(date "+%m-%d-%Y").csv"
+                rm -rf "${OUTPUT_DIR}/${filetype}/tmp"
+            fi
+        done
+        tp_doc_classifier.py -c ${MODELS_DIR}/${datatype}.pkl -p "${DATA_DIR}/${filetype}/tmp" -f ${cas_type} >> "${OUTPUT_DIR}/${filetype}/$(date "+%m-%d-%Y").csv"
+        rm -rf "${OUTPUT_DIR}/${filetype}/tmp"
+    done
 done
